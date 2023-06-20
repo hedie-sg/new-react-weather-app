be5041c6b0b36b9360b885d122e04b1d4714d527
hedie-sg <hedi84.ghasemi@gmail.com>
2023-06-20 20:52:26 +0300

    added the github url

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">Weather app</header>
+            <footer>
+                Coded by Hedi.sgh and is{' '}
+                <a
+                    href="https://github.com/hedie-sg/new-react-weather-app"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    open-sourced
+                </a>
+            </footer>
         </div>
     );
 }
