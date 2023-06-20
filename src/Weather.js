0644338ae59b27e098ef70833217cd23eb739888
hedie-sg <hedi84.ghasemi@gmail.com>
2023-06-20 23:42:52 +0300

    added styles of the app

--- src/Weather.js
+++ src/Weather.js
@@ -9,7 +9,7 @@ export default function weather() {
                     <div className="col-9">
                         <input
                             type="search"
-                            placeholder="Enter a city"
+                            placeholder="Enter a city..."
                             className="form-control"
                         />
                     </div>
@@ -17,27 +17,37 @@ export default function weather() {
                         <input
                             type="submit"
                             value="Search"
-                            className="btn btn-primary"
+                            className="btn btn-primary w-100"
                         />
                     </div>
                 </div>
             </form>
-            <h1>Tehran</h1>
-            <div className="row">
+            <h1 className="city">Tehran, Iran</h1>
+            <div className="row mt-3 mb-3">
                 <div className="col-6">
                     <ul>
-                        <li>Saturday 09:45</li>
-                        <li>mostly Cloudy</li>
-                        <li>Humidity: 50%</li>
-                        <li>Wind: 5km/h</li>
+                        <li>
+                            Saturday, <span className="date"> 09:45</span>
+                        </li>
+                        <li className="description">Mostly Cloudy</li>
+                        <li>
+                            Humidity: <span className="humidity">50%</span>
+                        </li>
+                        <li>
+                            Wind: <span className="wind">5km/h</span>
+                        </li>
                     </ul>
                 </div>
                 <div className="col-6">
                     <img
                         src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAdVJREFUaN7tmc1thDAQRimBElwCJVBCSvAxR5fgEiiBEiiBErhyIx24A2cc2WhiAf4ZA1rJkZ4UZZPN9/AwHrON1rr5ZJoqUAWqQBWoAlWgxJf++WaAAGZAAdpD2dfM7zDS/yopAGE6YDoIHMLIdK8KQIAWGIAtQ8Bh/r59bQWQjCBILCkSJIF1XVuAA9Jivm9ROd0ukS0AQTtgA7SH+Vn31EoEBSAMA2YUUAHiJDyWcCtBuidIArZEroJewVEpjQSJjiIgMsMbpHdjf53sCcEWSxEYCQKOyZQhkshZBZYkYEtHeLVPQSGJnHIS0QI2/FIo+L+VILTXOUVA3BD+D3Q/pAqoFIEebUxFQQLJN/Ojo0TEqDG/JgBv1hdgeVNAP4CKPSvkCKiCQc1KSMRs2+x902hO/Z4cYFhgWOQHY8zo9hOKgCCGH71BEXcqHjEBKDft5gowypVH4YeLgKE9ZSO10cxz7z7TFJqxOEUgZxyYbPi+0M4uSRuZPYCnCPBA6TwrYCWWyFbJImo/FTMpM6pAG5CYvDO0LDii7x2JNAtdSGxuQyp41Q87UqkHW8NJzYsbw+8d6Y5Hi+7qbw8IyOIPd9HRVD8qUD8fqAJVoApUgSrwqfwCJ6xaZshM+xMAAAAASUVORK5CYII="
                         alt=" Mostly cloudy"
+                        className="icon"
                     />
-                    25°C | °F
+                    <span className="temperature">25</span>
+                    <span className="unit">
+                        <a href="/">°C</a> | <a href="/">°F</a>
+                    </span>
                 </div>
             </div>
         </div>
