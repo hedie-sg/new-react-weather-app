161398727cefd7754939ba9a66a5321a1e74f739
hedie-sg <hedi84.ghasemi@gmail.com>
2023-06-20 21:23:07 +0300

    added bootstrap and axios

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-            <header className="App-header">Weather app</header>
-            <footer>
-                Coded by Hedi.sgh and is{' '}
-                <a
-                    href="https://github.com/hedie-sg/new-react-weather-app"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    open-sourced
-                </a>
-            </footer>
+            <div className="container">
+                <header className="App-header">Weather app</header>
+                <footer>
+                    Coded by Hedi.sgh and is{' '}
+                    <a
+                        href="https://github.com/hedie-sg/new-react-weather-app"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        open-sourced
+                    </a>
+                </footer>
+            </div>
         </div>
     );
 }
