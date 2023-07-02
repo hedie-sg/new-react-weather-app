24e03acd1df7c538e1756784d40d459ec995ba97
hedie-sg <hedi84.ghasemi@gmail.com>
2023-07-03 00:46:12 +0300

    make forecast integration

--- src/WeatherForcast.js
+++ src/WeatherForcast.js
@@ -1,29 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import WeatherIcon from './WeatherIcon';
 import axios from 'axios';
 import './App.css';
 
 export default function WeatherForcast(props) {
+    let [loaded, setLoaded] = useState(false);
+    let [forcast, setForcast] = useState(null);
+
     function handleResponse(response) {
-        console.log(response.data);
+        setForcast(response.data.daily);
+        setLoaded(true);
     }
-    console.log(props);
-    let apiKey = '2o0f65b4a41b4138505bc7t3f2fa0460';
-    let city = props.city;
-    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-    return (
-        <div className="WeatherForcast">
-            <div className="row">
-                <div className="col">
-                    <div className="forcast-day">Thu</div>
-                    <WeatherIcon code="snow-night" size={33} />
-                    <div className="forcast-temp">
-                        <span className="forcast-temp-max">15°</span>
-                        <span className="forcast-temp-min">10°</span>
+    function day() {
+        let date = new Date(forcast[0].time * 1000);
+        let day = date.getDay();
+
+        let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+        return days[day];
+    }
+
+    if (loaded) {
+        return (
+            <div className="WeatherForcast">
+                <div className="row">
+                    <div className="col">
+                        <div className="forcast-day">{day()}</div>
+                        <WeatherIcon
+                            code={forcast[0].condition.icon}
+                            size={33}
+                        />
+                        <div className="forcast-temp">
+                            <span className="forcast-temp-max">
+                                {Math.round(forcast[0].temperature.maximum)}°
+                            </span>
+                            <span className="forcast-temp-min">
+                                {Math.round(forcast[0].temperature.minimum)}°
+                            </span>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    } else {
+        let apiKey = '2o0f65b4a41b4138505bc7t3f2fa0460';
+        let city = props.city;
+        let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+
+        axios.get(apiUrl).then(handleResponse);
+        return null;
+    }
 }
